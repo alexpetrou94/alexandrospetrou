fc3ccb831d2241b2a254fe5761641b8876553f09
Alexandros Petrou <alexpetrou94@gmail.com>
2022-04-26 12:04:22 +0200

    Add navbar and home section

---
+++ wwwroot/js/InteropHelper.js
@@ -0,0 +1,43 @@
+﻿function sendAlert(msg) {
+    alert(msg);
+}
+
+function addClassByID(id, pClass) {
+    const element = document.getElementById(id);
+    element.classList.add(pClass);
+}
+
+function addClassByClass(elementClass, pClass) {
+    const elements = document.getElementsByClassName(elementClass);
+
+    for(var i = 0; i < elements.length; i++)
+    {
+        elements[i].classList.add(pClass);
+    }
+}
+
+function toggleClassByID(id, pClass) {
+    const element = document.getElementById(id);
+    element.classList.toggle(pClass);
+}
+
+function toggleClassByClass(elementClass, pClass) {
+    const elements = document.getElementsByClassName(elementClass);
+
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.toggle(pClass);
+    }
+}
+
+function removeClassByID(id, pClass) {
+    const element = document.getElementById(id);
+    element.classList.remove(pClass);
+}
+
+function removeClassByClass(elementClass, pClass) {
+    const elements = document.getElementsByClassName(elementClass);
+
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.remove(pClass);
+    }
+}
