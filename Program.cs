247ebc9feaf9a794a61515f5548400dd96ba36cb
Alexandros Petrou <alexpetrou94@gmail.com>
2022-05-01 17:47:57 +0200

    Add touch-ups

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
-using alexandrospetrou.Data;
 using alexandrospetrou.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
@@ -8,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddTransient<JSService>();
 
 var app = builder.Build();
